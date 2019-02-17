1cdf9254384f3af1528f7286e78504976ad4bd4e
Rémy Raes <raes.remy@gmail.com>
2019-02-17 19:18:22 +0100

    [feat] adding bootstrap to article form

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,11 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('text')
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('text', TextareaType::class,
+                array('attr' =>
+                    array('class' => 'form-control',
+                    'style' => 'min-height:100px')))
         ;
     }
 
@@ -21,6 +26,7 @@ class ArticleType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Article::class,
+            'attr' => array('style' => 'display:inline')
         ]);
     }
 }
